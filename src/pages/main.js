3e7b9fa90458c387023a1d59205ef57fa007fa75
Willian <wsilvadev@gmail.com>
2019-08-13 20:04:59 -0300

    learning react-native

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,16 +1,45 @@
+import api from '../services/api'
 import React, {Component} from 'react';
 import {
-    Text,View
+    Text,View,FlatList
 } from 'react-native';
 
+state = {
+
+    docs:[],
+};
 export default class Main extends Component{
 static navigationOptions ={
     title: 'JSHunte',
 }
+
+componentDidMount(){
+    this.loadProducts();
+}
+ loadProducts = async () =>{
+    const response = await api.get('/products');
+
+    const{docs} = response.data;
+console.log(docs)
+    this.setState({docs: this.state.docs});
+
+}
+renderItem = ({item}) =>(
+    <View>
+    <Text>{item.title}</Text>
+    <Text>{item.description}</Text>
+
+    </View>
+)
     render(){
         return(
             <View>
-                <Text>Funcionou</Text>
+                <FlatList
+                data={this.state.title}
+                keyExtractor={item =>  item._id}
+                renderItem={this.renderItem}
+                />
+
             </View>
         );
     }
