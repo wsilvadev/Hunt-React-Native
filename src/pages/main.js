74cb96c72f87d937f46270e6f7d315ab7cedb507
Willian <wsilvadev@gmail.com>
2019-08-14 23:26:22 -0300

    learning scroll infinit

--- src/pages/main.js
+++ src/pages/main.js
@@ -4,31 +4,46 @@ import {
     Text,View,FlatList,TouchableOpacity
 } from 'react-native';
 import Style from './style'
+
 export default class Main extends Component{
     static navigationOptions ={
         title: 'JSHunte',
     };
     
     state = {
-           docs:[]
+        productInfo:{},
+           docs:[],
+           page: 1
     };
 
 componentDidMount(){
     this.loadProducts();
 }
- loadProducts = async () =>{
-    const response = await api.get('/products');
-    const{docs} = response.data;
+ loadProducts = async (page = 1) =>{
+    const response = await api.get(`./products?page=${page}`);
+  
+    const{docs, ...productInfo} = response.data;
     
-    this.setState({docs});
+    this.setState({docs: [...this.state.docs, ...docs], productInfo, page});
 
+};
+loadMore = () => {
+    const{page, productInfo} = this.state;
+
+    if(page == productInfo.pages) return;
+    
+    const newPages = page + 1;
+    this.loadProducts(newPages);
 }
 renderItem = ({item}) =>{
     return (
         <View style = {Style.productContainer}>
             <Text style = {Style.productTitle}>{item.title}</Text>
             <Text style = {Style.productDescription}>{item.description}</Text>
-            <TouchableOpacity style = {Style.productButton} onPress = {()=>{}} >
+            <TouchableOpacity style = {Style.productButton} onPress = {()=>{
+                this.props.navigation.navigate(
+                    "Product", { product: item })
+            }} >
                 <Text style = {Style.productButtonText}>Acessar</Text>
             </TouchableOpacity>
 
@@ -45,6 +60,8 @@ renderItem = ({item}) =>{
                 data = {this.state.docs}
                 keyExtractor ={item => item._id}
                 renderItem = {this.renderItem}
+                onEndReached = {this.loadMore}
+                onEndReachedThreshold={0.1}
                 />
             </View>
         );
--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
     
   import { createStackNavigator } from 'react-navigation';
   import Main from './pages/main';
-
+  import Product from './pages/Product'
   export default createStackNavigator({
       Main,
+      Product,
   }, {
     navigationOptions: {
       headerStyle:{
